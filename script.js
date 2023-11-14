835723eb9e742602c647c8bf74032ebcfd919460
Silvan Stuber <silvan.stuber93@gmail.com>
2023-11-14 23:27:31 +0100

    style of the circle and cross revised

--- script.js
+++ script.js
@@ -15,63 +15,50 @@ function init() {
 }
 
 function createAnimatedCircle() {
-    // Erstellen des SVG-Elements
     const svgNS = "http://www.w3.org/2000/svg";
     let svg = document.createElementNS(svgNS, "svg");
-    svg.setAttribute("width", "70px");
-    svg.setAttribute("height", "70px");
-
-    // Erstellen des Kreis-Elements
+    svg.setAttribute("width", "60px");
+    svg.setAttribute("height", "60px");
     let circle = document.createElementNS(svgNS, "circle");
-    circle.setAttribute("cx", "35");
-    circle.setAttribute("cy", "35");
-    circle.setAttribute("r", "30");
+    circle.setAttribute("cx", "30");
+    circle.setAttribute("cy", "30");
+    circle.setAttribute("r", "25");
     circle.setAttribute("fill", "none");
     circle.setAttribute("stroke", "#00B0EF");
     circle.setAttribute("stroke-width", "5");
-    circle.setAttribute("stroke-dasharray", "188.4"); // Umfang des Kreises
-    circle.setAttribute("stroke-dashoffset", "188.4");
-
-    // Hinzufügen der Animation
-    circle.style.animation = "fillCircle 2s ease forwards";
-
-    // Hinzufügen des Kreises zum SVG-Element
+    circle.setAttribute("stroke-dasharray", "157");
+    circle.setAttribute("stroke-dashoffset", "157");
+    circle.classList.add("circle-animation");
     svg.appendChild(circle);
-
-    // Rückgabe des SVG-Elements als HTML-String
     return svg.outerHTML;
 }
 
 function createCross() {
     const svgNS = "http://www.w3.org/2000/svg";
     let svg = document.createElementNS(svgNS, "svg");
-    svg.setAttribute("width", "70px");
-    svg.setAttribute("height", "70px");
-
-    // Erstellen der Linien für das Kreuz
+    svg.setAttribute("width", "60px");
+    svg.setAttribute("height", "60px");
     let line1 = document.createElementNS(svgNS, "line");
-    line1.setAttribute("x1", "10");
-    line1.setAttribute("y1", "10");
-    line1.setAttribute("x2", "60");
-    line1.setAttribute("y2", "60");
+    line1.setAttribute("x1", "5");
+    line1.setAttribute("y1", "5");
+    line1.setAttribute("x2", "55");
+    line1.setAttribute("y2", "55");
     line1.setAttribute("stroke", "#ffc000");
     line1.setAttribute("stroke-width", "5");
-
+    line1.classList.add("cross-animation");
     let line2 = document.createElementNS(svgNS, "line");
-    line2.setAttribute("x1", "60");
-    line2.setAttribute("y1", "10");
-    line2.setAttribute("x2", "10");
-    line2.setAttribute("y2", "60");
+    line2.setAttribute("x1", "55");
+    line2.setAttribute("y1", "5");
+    line2.setAttribute("x2", "5");
+    line2.setAttribute("y2", "55");
     line2.setAttribute("stroke", "#ffc000");
     line2.setAttribute("stroke-width", "5");
-
-    // Hinzufügen der Linien zum SVG-Element
+    line2.classList.add("cross-animation");
     svg.appendChild(line1);
     svg.appendChild(line2);
-
-    // Rückgabe des SVG-Elements als HTML-String
     return svg.outerHTML;
 }
+
 
 function render() {
     let content = document.getElementById('content');
@@ -98,5 +85,4 @@ function render() {
 
     tableHTML += '</table>';
     content.innerHTML = tableHTML;
-}
-
+} 
