2172c515a8329ac72f723c76b4df60d6fbed14c6
Silvan Stuber <silvan.stuber93@gmail.com>
2023-11-15 21:05:43 +0100

    Determine winners

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 let fields = [
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
+    null, null, null,
+    null, null, null,
+    null, null, null
 ];
-
-function init() {
-    render();
-}
 
 let currentPlayer = 'circle';
 
@@ -24,14 +14,17 @@ function init() {
 function cellClicked(event) {
     const cellIndex = event.target.dataset.index;
 
-    if (fields[cellIndex] === null) {
+    if (fields[cellIndex] === null && !checkGameOver()) {
         fields[cellIndex] = currentPlayer;
         updateCell(cellIndex);
 
-        currentPlayer = (currentPlayer === 'circle') ? 'cross' : 'circle';
+        if (!checkGameOver()) {
+            currentPlayer = (currentPlayer === 'circle') ? 'cross' : 'circle';
+        } else {
+            disableClickHandlers();
+        }
     }
 }
-
 
 function createCircle() {
     const svgNS = "http://www.w3.org/2000/svg";
@@ -85,7 +78,89 @@ function updateCell(index) {
     } else if (fields[index] === 'cross') {
         cell.innerHTML = createCross();
     }
-    cell.onclick = null; // Entfernt den Click-Handler nach einem Klick
+}
+
+function checkGameOver() {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (const combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (fields[a] && fields[a] === fields[b] && fields[a] === fields[c]) {
+            drawWinningLine(combination);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function disableClickHandlers() {
+    const cells = document.querySelectorAll('td');
+    cells.forEach(cell => {
+        cell.onclick = null;
+    });
+}
+
+function drawWinningLine(combination) {
+    const content = document.getElementById('content');
+    const firstCell = document.querySelector(`td[data-index='${combination[0]}']`);
+    const lastCell = document.querySelector(`td[data-index='${combination[2]}']`);
+
+    const firstCellRect = firstCell.getBoundingClientRect();
+    const lastCellRect = lastCell.getBoundingClientRect();
+
+    const line = document.createElement("div");
+    line.id = "winning-line";
+    
+    // Anpassen der Linie je nach Orientierung (horizontal, vertikal, diagonal)
+    if (combination[0] % 3 === 0 && combination[2] === combination[0] + 2) {
+        // Horizontal
+        line.style.width = `${lastCellRect.right - firstCellRect.left}px`;
+        line.style.height = '5px';
+        line.style.top = `${firstCellRect.top + firstCellRect.height / 2}px`;
+        line.style.left = `${firstCellRect.left}px`;
+    } else if (combination[0] < 3 && combination[2] > 5) {
+        // Vertikal
+        line.style.width = '5px';
+        line.style.height = `${lastCellRect.bottom - firstCellRect.top}px`;
+        line.style.top = `${firstCellRect.top}px`;
+        line.style.left = `${firstCellRect.left + firstCellRect.width / 2}px`;
+    } else {
+        // Diagonal
+        // Hier müssen Sie die Logik für diagonale Linien anpassen
+    }
+
+    document.body.appendChild(line); // Füge die Linie dem Body hinzu, nicht dem #content
+}
+
+
+
+function createLine() {
+    const svgNS = "http://www.w3.org/2000/svg";
+    let svg = document.createElementNS(svgNS, "svg");
+    svg.setAttribute("class", "winning-line");
+    svg.setAttribute("width", "80");
+    svg.setAttribute("height", "80");
+
+    let line = document.createElementNS(svgNS, "line");
+    line.setAttribute("x1", "0");
+    line.setAttribute("y1", "0");
+    line.setAttribute("x2", "80");
+    line.setAttribute("y2", "80");
+    line.setAttribute("stroke", "red");
+    line.setAttribute("stroke-width", "5");
+
+    svg.appendChild(line);
+    return svg.outerHTML;
 }
 
 function render() {
